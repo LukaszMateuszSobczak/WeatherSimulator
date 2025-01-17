b6451b9db50a0bd0bf16fdb61e42048e1a87e5c6
lukasz <lukasz.sobczak.1995@gmail.com>
2025-01-17 23:39:41 +0100

    some logic

--- WeatherSimulator/Program.cs
+++ WeatherSimulator/Program.cs
@@ -5,11 +5,35 @@
         static void Main(string[] args)
         {
             Console.Write("Enter the number of days to simulate: ");
-            var days = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
 
             int[] temperatures = new int[days];
             string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
             string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+
+            for (int i = 0; i < days; i++)
+            {
+                temperatures[i] = random.Next(-25, 41);
+                if (temperatures[i] >= 2)
+                {
+                    weatherConditions[i] = conditions[random.Next(0, conditions.Length - 2)];
+                } else
+                {
+                    weatherConditions[i] = conditions[random.Next(0, conditions.Length - 1)];
+                }
+            }
+
+            foreach (int temp in temperatures)
+            {
+                Console.WriteLine(temp);
+            }
+
+            foreach(string weatherCondition in weatherConditions)
+            {
+                Console.WriteLine(weatherCondition);
+            }
 
         }
     }
